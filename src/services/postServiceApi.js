3d64d342275ea6a075e5d2ff82315353dc16be99
kashif tufail <kashif.tufail17@gmail.com>
2023-06-21 18:48:53 +0500

    create post mutation

--- src/services/postServiceApi.js
+++ src/services/postServiceApi.js
@@ -2,15 +2,29 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const postServerApi = createApi({
   reducerPath: 'postServerApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
+
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001' }),
+  tagTypes: ['Posts'],  
   endpoints: (builder) => ({
     getPosts: builder.query({
-      query: (page = 1) => '/posts',
+      query: (page = 1) => `api/v1/posts?_page=${page}&_limit=10`,
+      providesTags: ['Posts'],
     }),
     getSinglePost: builder.query({
-      query: (id) => `/posts/${id}`,
+      query: (id) => `/posts/${id}`,      
+    }),
+
+    createPost: builder.mutation({
+      query: (body) => ({
+        
+        url: 'api/v1/posts',
+        method: 'POST',
+        body: body,
+      }),
+      invalidatesTags: ['Posts'],
+
     }),
   }),
 });
 
-export const { useGetPostsQuery, useGetSinglePostQuery } = postServerApi;
+export const { useCreatePostMutation, useGetPostsQuery, useGetSinglePostQuery } = postServerApi;
